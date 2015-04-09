76c5b45871c974a0fd8b0904a4469d020533d5b0
Brian Getting <brian@tatem.ae>
2015-04-09 16:49:54 -0700

    Adding JS and vendoring Jquery

---
+++ js/tatemae.js
@@ -0,0 +1,21 @@
+var counter = 0;
+var services = [
+  "Accessibility",
+  "Email Marketing",
+  "E-commerce",
+  "Ruby",
+  "Ruby on Rails",
+  "API's",
+  "Shopify",
+  "Internet Consulting",
+];
+
+function rotateServices() {
+  $(".js-services").text(services[counter++]);
+  if (counter >= services.length) {counter = 0};
+}
+
+$(document).ready(function() {
+  $('body').fadeIn({duration: 3000});
+  setInterval(rotateServices, 5000);
+});
