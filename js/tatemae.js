382da5221635cbafe389561ad38f75a6dda9b20e
Brian Getting <brian@tatem.ae>
2015-10-24 20:10:53 -0700

    Updating tatem.ae site

--- js/tatemae.js
+++ js/tatemae.js
@@ -1,21 +1,27 @@
-var counter = 0;
-var services = [
-  "Accessibility",
-  "Email Marketing",
-  "E-commerce",
-  "Ruby",
-  "Ruby on Rails",
-  "API's",
-  "Shopify",
-  "Internet Consulting",
-];
+function reposition() {
+  window_height = $(window).height();
+  window_width = $(window).width();
+  one_sixth = window_height/6;
 
-function rotateServices() {
-  $(".js-services").text(services[counter++]);
-  if (counter >= services.length) {counter = 0};
+  aspect_ratio = 16 / 9; // Must match aspect ratio of background image
+  window_aspect_ratio = window_width / window_height;
+  if (window_aspect_ratio > aspect_ratio) {
+    $('.container').css('background-size', window_width + 'px auto');
+  } else {
+    $('.container').css('background-size', 'auto ' + window_height + 'px');
+  }
+
+  $('main').css('margin-top', one_sixth + 'px');
+
+  $btn = $('.btn')
+  $btn.css('margin-left', '-' + $btn.width()/2 + 'px');
+  $btn.css('bottom', one_sixth + 'px');
 }
 
 $(document).ready(function() {
-  $('main').fadeIn({duration: 3000});
-  setInterval(rotateServices, 5000);
+  Pace.on('done', function() {
+    $('#bg-tmp, #bg-overlay').addClass('faded');
+  });
+  reposition();
+  $(window).resize(reposition);
 });
